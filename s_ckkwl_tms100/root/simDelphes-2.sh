3311157527934cdd32832a16dfa57bfc8ccd02c2
YW-Hsiao <hsiao.phys@gapp.nthu.edu.tw>
2021-10-02 16:35:06 +0800

    add xsec tms50 100

---
+++ s_ckkwl_tms100/root/simDelphes-2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Program:
+#     This program is to do the Delphes simultaion about s-channel with CKKW-L.
+#     I copy the /root/Delphes-3.4.2/cards/delphes_card_ATLAS.tcl
+# Author: You-Wei Hsiao
+# Institute: Department of Physics, National Tsing Hua University, Hsinchu, Taiwan
+# Mail: hsiao.phys@gapp.nthu.edu.tw
+# History: 2021/10/01
+#     First release
+# Version: v.1.0
+
+
+# Setup environment and variables
+now=$(date)
+path_sChCkkwl="/youwei_home/SVJ_CKKWL/s_ckkwl_tms100"
+path_hepmc="$path_sChCkkwl/hepmc"
+path_root="$path_sChCkkwl/root"
+path_Delphes="/root/Delphes-3.4.2"
+
+
+# Check your .tcl file
+# Check you have executed "make"!!
+echo "Start Running .sh"
+echo "date: $now"
+echo "Path of script: $path_root"
+echo "Path of Delphes: $path_Delphes"
+echo "Path of .hepmc file: $path_hepmc"
+echo "Path of .tcl, .log, and .root files: $path_root"
+
+cd $path_Delphes
+nohup ./DelphesHepMC $path_root/delphes_card_ATLAS.tcl $path_root/svj_s_ckkwl-2.root $path_hepmc/ckkwl-2.hepmc > $path_root/svj_s_ckkwl-2.log 2>&1 &
---
+++ s_ckkwl_tms50/DP8/simDP8-8.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Program:
+#     This program is use DelphesPythia8 to do the Pythia and Delphes simultaion
+#     about s-channel with CKKW-L.
+#     Main:numberOfEvents = 10000, Random:seed = 100
+#     Merging:doKTMerging = on, Merging:Process = pp>{xd,4900101}{xd~,-4900101}
+#     Merging:mayRemoveDecayProducts = on
+#     Merging:TMS = 50
+# Author: You-Wei Hsiao
+# Institute: Department of Physics, National Tsing Hua University, Hsinchu, Taiwan
+# Mail: hsiao.phys@gapp.nthu.edu.tw
+# History: 2021/10/01
+#     First release
+# Version: v.1.0
+
+# Setup environment and variables
+now=$(date)
+path_sChCkkwl="/youwei_home/SVJ_CKKWL/s_ckkwl_tms50"
+path_DP8="$path_sChCkkwl/DP8"
+path_Delphes="/root/Delphes-3.4.2"
+
+
+# Check your .cmnd file Beams:LHEF path, number of events, and random seed!!   ###
+# Check your .tcl file  ###
+echo "Start Running"
+echo "date: $now"
+echo "Path of scrpit: $path_DP8"
+echo "Path of DelphesPythia8: $path_Delphes"
+echo "Path of .cmnd, .tcl, .log, and .root files: $path_DP8"
+
+cd $path_Delphes
+nohup ./DelphesPythia8 $path_DP8/delphes_card_ATLAS.tcl $path_DP8/svj_ckkwl-8.cmnd $path_DP8/svj_ckkwl-8.root > $path_DP8/svj_ckkwl-8.log 2>&1 &
